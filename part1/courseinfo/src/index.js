7dcfd8e3e959d5bbfe29c6a29eff562a4fd2fbc4
villichili <sarkka.mikko@gmail.com>
2020-10-04 14:02:50 +0300

    1.2 course info step2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -19,16 +19,22 @@ const Header = (props) => (
  */
 const Content = (props) => (
     <>
-      <p>
-        {props.part1} {props.exercise1}
-      </p>
-      <p>
-        {props.part2} {props.exercise2}
-      </p>
-      <p>
-        {props.part3} {props.exercise3}
-      </p>
+      <Part part={props.part1} exercise={props.exercise1}/>
+      <Part part={props.part2} exercise={props.exercise2}/>
+      <Part part={props.part3} exercise={props.exercise3}/>
     </>
+)
+
+/**
+ * Creates a part component
+ * @param props
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const Part = (props) => (
+    <p>
+      {props.part} {props.exercise}
+    </p>
 )
 
 /**

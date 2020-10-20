227b786556a01f97ee0f258be1f5c624951df3ff
villichili <sarkka.mikko@gmail.com>
2020-10-20 19:21:48 +0300

    Part 2.4: Course information step9

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -23,7 +23,7 @@ const Part = ({part}) => {
 }
 
 const Content = ({course}) => {
-  const parts = course.parts.map(p => <Part id={p.id} part={p}/>)
+  const parts = course.parts.map(p => <Part key={p.id} part={p}/>)
   return (
       <div>
         {parts}
@@ -42,34 +42,52 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
-
-  return <Course course={course}/>
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  const courseList = courses.map(c => <Course key={c.id} course={c}/>)
+  return <div>{courseList}</div>
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'))

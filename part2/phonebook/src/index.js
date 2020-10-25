6693e38002218cf67210b1e31fe2e9131b565c62
villichili <sarkka.mikko@gmail.com>
2020-10-25 18:40:30 +0200

    2.6: The Phonebook Step1

---
+++ part2/phonebook/src/index.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    {name: 'Arto Hellas'}
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+
+  return (
+      <div>
+        <h2>Phonebook</h2>
+        <form onSubmit={addPerson}>
+          <div>
+            name: <input value={newName} onChange={handleNameChange}/>
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+          <div>debug: {newName}</div>
+        </form>
+        <h2>Numbers</h2>
+        {persons.map(p => <div key={p.name}>{p.name}</div>)}
+      </div>
+  )
+}
+
+export default App
+
+ReactDOM.render(<App/>, document.getElementById('root'))
